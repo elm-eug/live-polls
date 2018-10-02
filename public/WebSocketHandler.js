7b049a32e86b206471cf3dd7ff6e978ac8ab8eff
Bill St. Clair <billstclair@gmail.com>
2018-10-02 07:48:27 -0400

    Add WebSocketHandler.js, and the ports in the Elm code.
    I tested the JavaScript code in the Chrome debugging console.
    
    Next: encoder/decoder for the messages sent back and forth.

---
+++ public/WebSocketHandler.js
@@ -0,0 +1,136 @@
+// WebSocket port handler
+
+/* Install it with:
+
+    WebSocketHandler.subscribe(cmdPort, subPort)
+
+Supports three messages, through the cmdPort:
+
+    { tag: 'open', args: { key: <string>, message: <string> } }
+    { tag: 'close', args: { key: <string> } }
+    { tag: 'send', args: { key: <string>, message: <string> } }
+
+The key for the 'open' command may be omitted, and the url will be used as key.
+
+Sends back the following messages:
+
+    { tag: 'error', args: { key: <string>, message: <string> } }
+    { tag: 'connected', args: { key: <string>, message: <string> } }
+    { tag: 'received', args: { key: <string>, message: <string> } }
+    { tag: 'closed', args: { key: <string>, message: <string> } }
+
+*/
+
+var WebSocketHandler = {};
+
+(function() {
+  var sockets = {};
+  var send;
+
+  WebSocketHandler.subscribe = subscribe;
+  WebSocketHandler.dispatch = dispatch; // for debugging
+
+  function subscribe(cmdPort, subPort) {
+    send = subPort.send;
+    cmdPort.subscribe(function(command) {
+      if (typeof(command) == 'object') {
+        var tag = command.tag;
+        var args = command.args;
+        dispatch(tag, args);
+      }
+    });
+  }
+
+  function returnObject(tag, args) {
+    send({tag: tag, args: args});
+  }
+
+  function returnError(key, err) {
+    returnObject('error',
+                 { key: key,
+                   message: err
+                 });
+  }
+
+  function dispatch(tag, args) {
+    if (tag == 'open') {
+      doOpen(args.key, args.message);
+    } else if (tag == 'close') {
+      doClose(args.key);
+    } else if (tag == 'send') {
+      doSend(args.key, args.message);
+    } else {
+      returnError('', 'Unknown tag: ' + tag);
+    }
+  }
+
+  function doOpen(key, url) {
+    if (!key) key = url;
+    if (sockets[key]) {
+      return returnError(key, 'Socket already open.');
+    }
+    try {
+	  var socket = new WebSocket(url);
+      sockets[key] = socket;
+    }
+    catch(err) {
+      // The old code returned BadSecurity if err.name was 'SecurityError'
+      // or BadArgs otherwise.
+      return returnError(key, "open: can't create socket for URL: " + url + ', ' + err.name);
+    }
+    socket.addEventListener('open', function(event) {
+      console.log('Socket connected for URL: ' + url);
+      returnObject('connected',
+                   { key: key,
+                     message: 'Socket connected for url: ' + url
+                   });
+    });
+    socket.addEventListener('message', function(event) {
+      var message = event.data;
+      console.log('Received for "' + key + '": ' + message);
+      returnObject('received',
+                   { key: key,
+                     message: message
+                   });
+    });
+    socket.addEventListener('close', function(event) {
+	  console.log('"' + key + '" closed');
+      delete sockets[key];        // for open errors
+      returnObject('closed',
+                   { key: key,
+                     message: "Socket closed unexpectedly."
+                   });
+    });
+  } 
+
+  function doSend(key, message) {
+    var socket = sockets[key];
+    if (!socket) {
+      returnError(key, 'send, socket not open.');
+    }
+    console.log('Sending message for key: ' + key + ', ' + message);
+    try {
+	  socket.send(message);
+    } catch(err) {
+      returnError(key, 'Send error: ' + err.name);
+    }
+  } 
+
+  function doClose(key) {
+    var socket = sockets[key];
+    if (!socket) {
+      returnError(key, 'close, socket not open.');
+    }
+    try {
+      delete sockets[key];
+      socket.close();
+      returnObject('closed',
+                   { key : key,
+                     message: "Socket closed."
+                   });
+    } catch(err) {
+      returnError(key, 'Close error: ' + err.name);
+    }
+  } 
+
+})();
